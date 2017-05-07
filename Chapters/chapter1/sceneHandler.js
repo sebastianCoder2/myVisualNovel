ffa09d4e110447dde857c26fb941a2f522709aa3
Sebastian Kawa <sebob16@gmail.com>
2017-05-07 16:09:58 +0200

    full story with music and complete save/load function

--- Chapters/chapter1/sceneHandler.js
+++ Chapters/chapter1/sceneHandler.js
@@ -1,21 +1,25 @@
         var counter = -1;
         var mydata = $.parseJSON(chapter1op0);
 		var decisionJson = $.parseJSON(choose1);
-        var gameover = false
+        var gameover = false;
 		var allowclick = 0;
 		var placeHolderText = 0;
 		var placeHolderName = 0;
         var musicCounter = 0;
         var musicSrc = $.parseJSON(music);
-
+		var ending1 = 0;
+        var ending2 = 0;
+        var ending3 = 0;
+		var musicSave;
+		var audio;
 $(document).ready(function() {
 audioplay();
 function audioplay(){
         
         
-        var audio = document.getElementById('playme');
-        audio.setAttribute("src", musicSrc[musicCounter].song1);
-        console.log(audio); 
+        audio = document.getElementById('playme');
+		audio.play();
+        audio.setAttribute("src", musicSrc[musicCounter].song);
         }
    
 //beim ersten laden faded szene rein	
@@ -57,47 +61,16 @@ $('body').contextmenu(function(e) {
         audioplay();
     }
 
-         console.log(counter);
-
+      
+			
+			setName();
+			setText();
             getBackground();
             getCharacter();
             
-        
-        
-        
-//fügt gesprochenen Text ein
-        var textAdder = setInterval(function(){
-            document.getElementById("text").innerHTML += mydata[counter].text.charAt(placeHolderText);
-            if (++placeHolderText == mydata[counter].text.length){
-                clearInterval(textAdder);
-                placeHolderText=0;
-            }else{
-            	$('body').on('keypress click',function(){
-
-            		clearInterval(textAdder);
-            		placeHolderText=0;
-            	});
-            }
-    }, 1);
-
-//fügt namen des characters ein
-        var nameAdder = setInterval(function(){
-            document.getElementById("personWhoTalk").innerHTML += mydata[counter].name.charAt(placeHolderName);
-            if (++placeHolderName == mydata[counter].name.length){
-                clearInterval(nameAdder);
-                placeHolderName=0;
-            }else{
-            	$('body').on('keypress click',function(){
-            		clearInterval(nameAdder);
-            		placeHolderName=0;
-            	});
-            }
-        }, 1);
 //wenn am ende eines json array
     	if (counter==mydata.length-1){
-            console.log("test + "+mydata[0].arrayName);
             var test = ($.inArray( "Jeff", mydata[0] ));
-            console.log("inarry +"+test);
 	//click deaktivert	
 			allowclick = 1;
     		var op1;
@@ -134,6 +107,7 @@ $('body').contextmenu(function(e) {
                     hideOptions();
                     gameover = true;
            });
+		   score();
 		}else if (mydata[0].arrayName === "chapter1op1" || mydata[0].arrayName === "chapter1op2"){
             $( "#option1" ).on('keypress click',function(){
                 allowclick = 0;
@@ -159,6 +133,189 @@ $('body').contextmenu(function(e) {
                     hideOptions();
                     gameover = true;
            });
+		   score();
+        }else if (mydata[0].arrayName === "chapter2op1" || mydata[0].arrayName === "chapter2op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    mydata = $.parseJSON(chapter3op1);
+                    decisionJson = $.parseJSON(choose4);
+                
+                counter=-1;
+                hideOptions(); 
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter3op2);
+                    decisionJson = $.parseJSON(choose4);
+                    counter=-1;
+                hideOptions();
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter3op3);
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
+        }else if (mydata[0].arrayName === "chapter3op1" || mydata[0].arrayName === "chapter3op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    mydata = $.parseJSON(chapter4op1);
+                    decisionJson = $.parseJSON(choose5);
+                
+                counter=-1;
+                hideOptions(); 
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter4op2);
+                    decisionJson = $.parseJSON(choose5);
+                    counter=-1;
+                hideOptions();
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter4op3);
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
+        }else if (mydata[0].arrayName === "chapter4op1" || mydata[0].arrayName === "chapter4op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    mydata = $.parseJSON(chapter5op1);
+                    decisionJson = $.parseJSON(choose6);
+                
+                counter=-1;
+                hideOptions(); 
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter5op2);
+                    decisionJson = $.parseJSON(choose6);
+                    counter=-1;
+                hideOptions();
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter5op3);
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
+        }else if (mydata[0].arrayName === "chapter5op1" || mydata[0].arrayName === "chapter5op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    mydata = $.parseJSON(chapter6op1);
+                    decisionJson = $.parseJSON(choose7);
+                
+                counter=-1;
+                hideOptions(); 
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter6op2);
+                    decisionJson = $.parseJSON(choose7);
+                    counter=-1;
+                hideOptions();
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter6op3);
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
+        }else if (mydata[0].arrayName === "chapter6op1" || mydata[0].arrayName === "chapter6op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    mydata = $.parseJSON(chapter7op1);
+                    decisionJson = $.parseJSON(choose8);
+                
+                counter=-1;
+                hideOptions(); 
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter7op2);
+                    decisionJson = $.parseJSON(choose8);
+                    counter=-1;
+                hideOptions();
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter7op3);
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
+        }else if (mydata[0].arrayName === "chapter7op1" || mydata[0].arrayName === "chapter7op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    mydata = $.parseJSON(chapter8op1);
+                    decisionJson = $.parseJSON(choose9);
+                
+                counter=-1;
+                hideOptions(); 
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter8op2);
+                    decisionJson = $.parseJSON(choose9);
+                    counter=-1;
+                hideOptions();
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                mydata = $.parseJSON(chapter8op3);
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
+        }else if (mydata[0].arrayName === "chapter8op1" || mydata[0].arrayName === "chapter8op2"){
+            $( "#option1" ).on('keypress click',function(){
+                allowclick = 0;
+                
+                    ending();
+                
+                counter=-1;
+                hideOptions(); 
+				gameover = true;
+            });
+            $( "#option2" ).on('keypress click',function(){
+            allowclick = 0;
+                ending();
+                    counter=-1;
+                hideOptions();
+				gameover = true;
+           });
+//game over
+            $( "#option3" ).on('keypress click',function(){
+            allowclick = 0;
+                ending();
+                counter=-1;
+                    hideOptions();
+                    gameover = true;
+           });
+		   score();
         }
          
     	}
@@ -204,16 +361,26 @@ function fillDecision(){
             allowclick = 0;
             mydata = $.parseJSON(localStorage.getItem('gameStorage'));
             decisionJson = $.parseJSON(localStorage.getItem('gamedecisionStorage'));
-            counter = mydata.length-1
+			musicSrc = $.parseJSON(localStorage.getItem('gameStoragemusicSrc'));
+			musicCounter = $.parseJSON(localStorage.getItem('gameStoragemusiccounter'));
+			ending1 = $.parseJSON(localStorage.getItem('ending1'));
+			ending2 = $.parseJSON(localStorage.getItem('ending2'));
+			ending3 = $.parseJSON(localStorage.getItem('ending3'));
+            counter = mydata.length-1;
+			audioplay();
         }
     }
 
     function speichern(){  
-        alert("Save complete");
     allowclick = 0;     
     window.localStorage.clear();      
 localStorage.setItem('gameStorage', JSON.stringify(mydata));
 localStorage.setItem('gamedecisionStorage', JSON.stringify(decisionJson));
+localStorage.setItem('gameStoragemusicSrc', JSON.stringify(musicSrc));
+localStorage.setItem('gameStoragemusiccounter', JSON.stringify(musicCounter));
+localStorage.setItem('ending1', JSON.stringify(ending1));
+localStorage.setItem('ending2', JSON.stringify(ending2));
+localStorage.setItem('ending3', JSON.stringify(ending3));
 
 $('#savebutton').hide();
 
@@ -228,6 +395,37 @@ $('#savebutton').hide();
             $('#option3').hide();
             $('#savebutton').hide();
     }
+function setName() {
+                //fügt namen des characters ein
+                var nameAdder = setInterval(function() {
+                    document.getElementById("personWhoTalk").innerHTML += mydata[counter].name.charAt(placeHolderName);
+                    if (++placeHolderName == mydata[counter].name.length) {
+                        clearInterval(nameAdder);
+                        placeHolderName = 0;
+                    } else {
+                        $('body').on('keypress click', function() {
+                            clearInterval(nameAdder);
+                            placeHolderName = 0;
+                        });
+                    }
+                }, 1);
+            }
+function setText() {
+                //fügt gesprochenen Text ein
+                var textAdder = setInterval(function() {
+                    document.getElementById("text").innerHTML += mydata[counter].text.charAt(placeHolderText);
+                    if (++placeHolderText == mydata[counter].text.length) {
+                        clearInterval(textAdder);
+                        placeHolderText = 0;
+                    } else {
+                        $('body').on('keypress click', function() {
+
+                            clearInterval(textAdder);
+                            placeHolderText = 0;
+                        });
+                    }
+                }, 1);
+            }
 //holt sich character aus dem Json und setzt ein
     function getCharacter(){       
     if (mydata[counter-1] != null){ 
@@ -253,9 +451,7 @@ $('#savebutton').hide();
      }
 //holt background aus Json und setzt ein
     function getBackground(){
-        console.log("test + "+mydata[0].arrayName);
         if (mydata[counter-1] != null){
-        console.log("bg"+mydata[counter-1].bgimg);
         if(mydata[counter].bgimg.length > 1){
             if(mydata[counter].bgimg == mydata[counter-1].bgimg){
                 var bg ="url(" + mydata[counter].bgimg + ")";
@@ -273,4 +469,30 @@ $('#savebutton').hide();
                 $("#background").css("background-image", bg);
         }
     }
+	function ending(){	
+                    var end1 = $.parseJSON(endingOne);
+                    var end2 = $.parseJSON(endingTwo);
+                    var end3 = $.parseJSON(endingThree);
+                    if (ending1 > ending2 && ending1 > ending3) {
+                        mydata = end1;
+                    } else if (ending2 > ending1 && ending2 > ending3) {
+                        mydata = end2;
+                    } else if (ending3 > ending1 && ending3 > ending3) {
+                        mydata = end3;
+                    } else {
+                        var endArray = [end1, end2, end3];
+                        var rand = endArray[Math.floor(Math.random() * endArray.length)];
+                        mydata = rand;
+                    }
+                
+}
+	function score() {
+                if (mydata[0].ending1 == true) {
+                    ending1++;
+                } else if (mydata[0].ending2 == true) {
+                    ending2++;
+                } else if (mydata[0].ending3 == true) {
+                    ending3++;
+                }
+            }
 });
